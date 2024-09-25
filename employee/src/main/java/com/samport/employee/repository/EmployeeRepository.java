111b6e8f996c92089585d3c4cbcc5a27be1c0da4
SamuelRathnaraj <samvelrathnaraj23@gmail.com>
2024-09-25 11:02:22 +0530

    task added

---
+++ employee/src/main/java/com/samport/employee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.samport.employee.repository;
+
+
+import com.samport.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
